97884ea49cebfb526ccd373c753213bab7f53b0e
kareem <karim.salah2048@gmail.com>
2016-08-15 18:37:35 +0200

    Changed The Start up Class

---
+++ Desktop Manger/data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_Manger
+{
+    class SaveData
+    {
+        public SaveData()
+        {
+
+        }
+
+        
+    }
+}
