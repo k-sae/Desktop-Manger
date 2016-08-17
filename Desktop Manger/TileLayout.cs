0dc3917be38c4b23b5464ce291b86b182b75398d
Ahmed Essam <Ahmed Essam>
2016-08-18 01:05:47 +0800

    Create TileLayout and Tile classes

---
+++ Desktop Manger/TileLayout.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Desktop_Manger
+{
+    class TileLayout : Panel
+    {
+        private double tileWidth;
+        private double tileHeight;
+        private double xSpacing;
+        private double ySpacing;
+        private List<Tile> children;
+        public double TileWidth
+        {
+            get
+            {
+                return tileWidth;
+            }
+            set
+            {
+                //TODO
+            }
+        }
+        public double TileHeight
+        {
+            get
+            {
+                return tileHeight;
+            }
+            set
+            {
+                //TODO
+            }
+        }
+        public double XSpacing
+        {
+            get
+            {
+                return xSpacing;
+            }
+            set
+            {
+                //TODO
+            }
+        }
+        public double YSpacing
+        {
+            get
+            {
+                return ySpacing;
+            }
+            set
+            {
+                //TODO
+            }
+        }
+
+        //Constructor
+        public TileLayout()
+        {
+            //TODO
+        }
+
+        public void AddTile(Tile tile)
+        {
+            //TODO
+        }
+
+        public void RemoveTile(Tile tile)
+        {
+            //TODO
+        }
+
+    }
+}
