28058503d990547ce0f870a53363e55c7a8dd4a3
kareem <karim.salah2048@gmail.com>
2017-12-25 21:52:35 +0200

    fix a rand bug where Dm suddenly return to the foreground state
    Fun Fact:
             the windows suddenly realizes that everything works fine
             so he decides to do some random stuff in order to * every thing up
    
    and thnx to mr windows i had to loop through all the running window classes
    in order to find the rand class he attached 'SHELLDLL_DefView' on :)

--- Desktop Manger/MainWindow.xaml.cs
+++ Desktop Manger/MainWindow.xaml.cs
@@ -110,17 +110,69 @@ namespace Desktop_Manger
 
         private void SetAsBackground()
         {
-            IntPtr hprog = FindWindowEx(
-            FindWindowEx(
-                FindWindow("Progman", "Program Manager"),
-                IntPtr.Zero, "SHELLDLL_DefView", ""
-                ),
-            IntPtr.Zero, "SysListView32", "FolderView"
-         );
-
-            SetWindowLong(new System.Windows.Interop.WindowInteropHelper(this).Handle, GWL_HWNDPARENT, hprog);
+            SetWindowLong(new System.Windows.Interop.WindowInteropHelper(this).Handle, GWL_HWNDPARENT, GetDesktopWindow(DesktopWindow.SysListView32));
+        }
+        public enum DesktopWindow
+        {
+            ProgMan,
+            SHELLDLL_DefViewParent,
+            SHELLDLL_DefView,
+            SysListView32
         }
 
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetShellWindow();
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+       private static StringBuilder Builder = new StringBuilder(1024);
+        public static IntPtr GetDesktopWindow(DesktopWindow desktopWindow)
+        {
+            IntPtr _ProgMan = GetShellWindow();
+            IntPtr _SHELLDLL_DefViewParent = _ProgMan;
+            IntPtr _SHELLDLL_DefView = FindWindowEx(_ProgMan, IntPtr.Zero, "SHELLDLL_DefView", null);
+            IntPtr _SysListView32 = FindWindowEx(_SHELLDLL_DefView, IntPtr.Zero, "SysListView32", "FolderView");
+
+            if (_SHELLDLL_DefView == IntPtr.Zero)
+            {
+                EnumWindows((hwnd, lParam) =>
+                {
+                    GetClassName(hwnd, Builder, Builder.Capacity);
+                    if (Builder.ToString() == "WorkerW")
+                    {
+                        IntPtr child = FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", null);
+                        if (child != IntPtr.Zero)
+                        {
+                            _SHELLDLL_DefViewParent = hwnd;
+                            _SHELLDLL_DefView = child;
+                            _SysListView32 = FindWindowEx(child, IntPtr.Zero, "SysListView32", "FolderView"); ;
+                            return false;
+                        }
+                    }
+                    return true;
+                }, IntPtr.Zero);
+            }
+
+            switch (desktopWindow)
+            {
+                case DesktopWindow.ProgMan:
+                    return _ProgMan;
+                case DesktopWindow.SHELLDLL_DefViewParent:
+                    return _SHELLDLL_DefViewParent;
+                case DesktopWindow.SHELLDLL_DefView:
+                    return _SHELLDLL_DefView;
+                case DesktopWindow.SysListView32:
+                    return _SysListView32;
+                default:
+                    return IntPtr.Zero;
+            }
+        }
         //make the app maximized at the center of the screen
         public void CenterScreen()
         {
